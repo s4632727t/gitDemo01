c6b0b86d77e7af1ddc58dd427a5fa5de964e53b0
kevin <kevin@126.com>
2022-07-14 20:34:51 +0800

    fourth commit pom and TestDemo

--- src/main/java/com/javaee/demo/TestDemo.java
+++ src/main/java/com/javaee/demo/TestDemo.java
@@ -4,5 +4,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("running...");
         System.out.println("running222...");
+        System.out.println("running333...");
     }
 }
