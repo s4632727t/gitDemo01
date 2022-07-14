5471835e547337d01abd6d58af271bb8be588000
kevin <kevin@126.com>
2022-07-14 17:41:51 +0800

    third commit pom and TestDemo

--- src/main/java/com/javaee/demo/TestDemo.java
+++ src/main/java/com/javaee/demo/TestDemo.java
@@ -3,5 +3,6 @@ package com.javaee.demo;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("running...");
+        System.out.println("running222...");
     }
 }
