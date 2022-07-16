d6870d4edd7484d829a9ae54cb9b8c3e442ee21a
s4632727t <88874108+s4632727t@users.noreply.github.com>
2022-07-16 14:36:06 +0800

    Update TestDemo.java

--- src/main/java/com/javaee/demo/TestDemo.java
+++ src/main/java/com/javaee/demo/TestDemo.java
@@ -12,5 +12,6 @@ public class TestDemo {
         System.out.println("function-game-test...");
         System.out.println("push test...");
         System.out.println("pull test...");
+        System.out.println("pull test222222222222...");
     }
 }
